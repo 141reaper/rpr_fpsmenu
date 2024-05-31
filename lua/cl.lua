98741e256c8e71f4db3bc4567423ec48a61c1739
141Reaper <150450172+141reaper@users.noreply.github.com>
2024-06-01 01:54:19 +0200

    Add files via upload

---
+++ lua/cl.lua
@@ -0,0 +1,88 @@
+RageUI.Banner(true, false)
+local function ClearGraphics(ped)
+    ClearAllBrokenGlass()
+    ClearAllHelpMessages()
+    LeaderboardsReadClearAll()
+    ClearBrief()
+    ClearGpsFlags()
+    ClearPrints()
+    ClearSmallPrints()
+    ClearReplayStats()
+    LeaderboardsClearCacheData()
+    ClearFocus()
+    ClearHdArea()
+    ClearPedBloodDamage(ped)
+    ClearPedWetness(ped)
+    ClearPedEnvDirt(ped)
+    ResetPedVisibleDamage(ped)
+    ClearOverrideWeather()
+    DisableScreenblurFade()
+    SetRainLevel(0.0)
+    SetWindSpeed(0.0)
+end
+
+local function SetLow()
+    local ped = PlayerPedId()
+    SetTimecycleModifier('exile1_plane')
+    ClearGraphics(ped)
+end
+
+
+local function SetLowTexture()
+    local ped = PlayerPedId()
+    SetTimecycleModifier('v_janitor')
+    ClearGraphics(ped)
+end
+
+local function SetNoGpu()
+    local ped = PlayerPedId()
+    SetTimecycleModifier('HicksbarNEW')
+    ClearGraphics(ped)
+end
+
+
+local function ResetGraphics()
+    SetTimecycleModifier()
+    ClearTimecycleModifier()
+    ClearExtraTimecycleModifier()
+end
+
+
+function fpsmenu()
+    local fpsmenu = RageUI.CreateMenu("", "FPS Verbessern", 0, 400, "fpsmenu", "fpsmenu")
+    RageUI.Visible(fpsmenu, not RageUI.Visible(fpsmenu))
+    while fpsmenu do
+        Wait(0)
+            RageUI.IsVisible(fpsmenu, true, true, true, function()
+                RageUI.ButtonWithStyle("Niedrige Einstellungen", nil, {RightLabel = nil}, true, function(Hovered, Active, Selected)
+                    if (Selected) then
+                        SetLow()
+                    end
+                end)
+                RageUI.ButtonWithStyle("Niedrige Texturen", nil, {RightLabel = nil}, true, function(Hovered, Active, Selected)
+                    if (Selected) then
+                        SetLowTexture()
+                    end
+                end)
+                RageUI.ButtonWithStyle("Scheiß PC (Mein Beileid)", nil, {RightLabel = nil}, true, function(Hovered, Active, Selected)
+                    if (Selected) then
+                        SetNoGpu()
+                    end
+                end)
+                RageUI.ButtonWithStyle("Grafik zurücksetzen", nil, {RightLabel = nil}, true, function(Hovered, Active, Selected)
+                    if (Selected) then
+                        ResetGraphics()
+                    end
+                end)
+			end, function()
+			end)
+              if not RageUI.Visible(fpsmenu) and not RageUI.Visible(fpsmenuSub) then
+              fpsmenu = RMenu:DeleteType("fpsmenu", true)
+        end
+    end
+end
+
+
+RegisterCommand('fpsmenu', function ()
+    fpsmenu()
+  end, false)
